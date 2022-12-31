ae797922bcc1553c34ca89d2c2e7a974bdc4b6dc
Nicolas Ospino <nicolasospino@Nicolass-MacBook-Air-2.local>
2022-12-31 13:57:27 -0500

    cambio colores

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ function toggleAnimation() {
   dark = !dark;
   let clone = big_wrapper.cloneNode(true);
   if (dark) {
-    clone.classList.remove("light");
-    clone.classList.add("dark");
-  } else {
     clone.classList.remove("dark");
     clone.classList.add("light");
+  } else {
+    clone.classList.remove("light");
+    clone.classList.add("dark");
   }
   clone.classList.add("copy");
   main.appendChild(clone);
